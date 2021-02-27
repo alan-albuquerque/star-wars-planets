11302078b3c2db3ee42cad6f285bc291e7b897b9
Alan Nascimento <alanteclog@gmail.com>
2021-02-27 17:39:37 -0300

    first commit

---
+++ src/test/java/com/alantech/starwarsplanets/StarWarsPlanetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alantech.starwarsplanets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsPlanetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
