d8b24a9d5016fca990fb9fa00fce38764af70165
Alan Nascimento <alanteclog@gmail.com>
2021-03-02 00:34:11 -0300

    add logging aspect to project

--- src/main/java/com/alantech/starwarsplanets/StarWarsPlanetsApplication.java
+++ src/main/java/com/alantech/starwarsplanets/StarWarsPlanetsApplication.java
@@ -1,9 +1,12 @@
 package com.alantech.starwarsplanets;
 
+import com.alantech.starwarsplanets.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({AppProperties.class})
 public class StarWarsPlanetsApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/alantech/starwarsplanets/config/AppProperties.java
@@ -0,0 +1,15 @@
+package com.alantech.starwarsplanets.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Star Wars Planets.
+ * <p>
+ * Properties are configured in the {@code application.yml} file.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+@Data
+public class AppProperties {
+	public static final String DEVELOPMENT_PROFILE = "dev";
+}
