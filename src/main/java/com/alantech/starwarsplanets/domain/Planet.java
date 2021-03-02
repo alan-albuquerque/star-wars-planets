c24e1ed359f434b0115ef65c4e6c1b77f097e2e5
Alan Nascimento <alanteclog@gmail.com>
2021-03-01 23:37:53 -0300

    create planet as a mongo document

---
+++ src/main/java/com/alantech/starwarsplanets/domain/Planet.java
@@ -0,0 +1,21 @@
+package com.alantech.starwarsplanets.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Document(collation = "planets")
+public class Planet extends AbstractAuditableDocument {
+	private String name;
+	private String climate;
+	private String terrain;
+}
