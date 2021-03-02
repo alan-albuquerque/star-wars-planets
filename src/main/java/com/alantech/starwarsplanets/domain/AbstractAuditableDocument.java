a82d80d94c11bef9ab89be8fe5e7d7da09b2e8c8
Alan Nascimento <alanteclog@gmail.com>
2021-03-02 00:31:20 -0300

    add call super to Planet ToString annotation

--- src/main/java/com/alantech/starwarsplanets/domain/AbstractAuditableDocument.java
+++ src/main/java/com/alantech/starwarsplanets/domain/AbstractAuditableDocument.java
@@ -4,11 +4,13 @@ import java.util.Date;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 public class AbstractAuditableDocument extends AbstractDocument {
 	@CreatedDate
 	private Date createdDate;
--- src/main/java/com/alantech/starwarsplanets/domain/Planet.java
+++ src/main/java/com/alantech/starwarsplanets/domain/Planet.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
@@ -13,6 +14,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 @Document(collation = "planets")
 public class Planet extends AbstractAuditableDocument {
 	private String name;
